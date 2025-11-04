73a315880c1fca1c5627641f978b14e7e8ca86d1
shubhashree500 <shreeshubha98@gmail.com>
2025-11-04 16:36:24 +0530

    check live

--- config/route.config.js
+++ config/route.config.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const apiBaseURL= 'http://localhost:6567/api/v1'
-// const apiBaseURL=`https://utkal-payrole-api-node.onrender.com/api/v1`
-// const apiBaseURL=`https://api.smartlink.ind.in/api/v1
+// const apiBaseURL= 'http://localhost:6567/api/v1'
+const apiBaseURL= 'https://api-new.mrcorporate.in/api/v1'
 // Create an Axios instance with your base URL
 const apiClient = axios.create({
   baseURL: apiBaseURL, // Your base API URL
